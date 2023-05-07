5a0fef3af8f584d56c4f8f75d50d1ea8afb16a8b
Kowal <113093445+Doubleduck61@users.noreply.github.com>
2023-05-07 21:52:34 +0200

    Update 055.py

--- 055.py
+++ 055.py
@@ -53,3 +53,50 @@ while i < length_abbreviation:
     i += 1
 
 print("".join(password))
+
+######################################
+
+import random
+import string
+
+lists = []
+quantity = random.randint(10,15)
+whole = 0
+a=0
+c=0
+while whole < quantity:
+    c+=1
+    if c>=5:
+        a=random.randint(1, 4)
+    elif c<=4:
+        a+=1
+        
+    if a == 1:
+        lists.extend(str(random.randint(2, 9)))
+        whole += 1
+
+    elif a == 2:
+        length=len(string.ascii_lowercase)
+        x=random.randint(0, length-1)
+        lists.extend(string.ascii_lowercase[x])
+        whole += 1
+
+    elif a == 3:
+        capital_letter=list(string.ascii_uppercase)
+        capital_letter.remove("O")
+        capital_letter.remove("I")
+        length=len(capital_letter)
+        x=random.randint(0, length-1)
+        lists.extend(capital_letter[x])
+        whole += 1
+
+    else:
+        signs=string.punctuation
+        length=len(signs)
+        x=random.randint(0, length-1)
+        lists.extend(signs[x])
+        whole += 1
+
+if whole == quantity:
+    password = "".join(lists)
+    print(password)
