93f585f76e811bc97cd098e5021aa48f689de32d
Kowal <113093445+Doubleduck61@users.noreply.github.com>
2023-05-07 15:51:16 +0200

    Update 055.py

--- 055.py
+++ 055.py
@@ -8,57 +8,48 @@ Hasło musi mieć między 10 a 15 znaków.
 Hasło nie może zawierać znaków "mylących", 1, I, O, 0.
 """
 import random
+import string
 
-lists=[ ]
-#3 losowe cyfry
-a = random.randint(0, 9)
-b = random.randint(0, 9)
-c = random.randint(0, 9)
-#3 losowe małe litery
-litery=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","w","x","y","z"]
-dlugosc_litery = len(litery)
-d1 = random.randint(0, dlugosc_litery-1)
-e1 = random.randint(0, dlugosc_litery-1)
-f1 = random.randint(0, dlugosc_litery-1)
+password = []
+a = 0 
+lists = []
 
-d = litery[d1]
-e = litery[e1]
-f = litery[f1]
+def random_digit(lists,a):
+    while a < 3:
+        lists.extend(str(random.randint(2, 9)))
+        a += 1
+    return lists
 
-#3 losowe duże litery
-g1 = random.randint(0, dlugosc_litery-1)
-h1 = random.randint(0, dlugosc_litery-1)
-i1 = random.randint(0, dlugosc_litery-1)
-g2 = litery[g1]
-g=g2.upper()
-h2 = litery[h1]
-h=h2.upper()
-i2 = litery[i1]
-i = i2.upper()
+def random_small_letter(lists,a):
+    signs = list(string.ascii_lowercase)
+    length = len(signs)
+    return while_loop(lists, a, length, signs)
 
-#Losowanie znaku
-znak=["!","@","#","$","%","^","&","*","(",")","_","-","=","+","{","}",",",">","<","/","?"]
-dlugosc_znak=len(znak)
-j1 = random.randint(0, dlugosc_znak-1)
-k1 = random.randint(0, dlugosc_znak-1)
-l1 = random.randint(0, dlugosc_znak-1)
-j = znak[j1]
-k = znak[k1]
-l = znak[l1]
-a=str(a)
-b=str(b)
-c=str(c)
-lists.extend(a)
-lists.extend(d)
-lists.extend(b)
-lists.extend(k)
-lists.extend(f)
-lists.extend(g)
-lists.extend(i)
-lists.extend(c)
-lists.extend(j)
-lists.extend(h)
-lists.extend(l)
-lists.extend(e)
-haslo="".join(lists)
-print(haslo)
+def random_big_letter(lists,a):
+    signs = list(string.ascii_uppercase)
+    signs.remove("O")
+    signs.remove("I")
+    length = len(signs)
+    return while_loop(lists, a, length, signs) 
+
+def special_signs(lists,a):
+    signs = list(string.punctuation)
+    signs.remove("#")
+    length = len(signs)
+    return while_loop(lists, a, length, signs)
+
+def while_loop(lists, a, length, signs):
+    while a < 3:
+        lists.extend(signs[random.randint(0, length-1)])
+        a += 1
+    lists=[]
+    return lists
+
+abbreviation = [random_digit(lists,a),random_small_letter(lists,a),random_big_letter(lists,a),special_signs(lists,a)]
+length_abbreviation = len(abbreviation)
+i = 0
+while i < length_abbreviation:
+    password.extend(abbreviation[i])
+    i += 1
+
+print("".join(password))
