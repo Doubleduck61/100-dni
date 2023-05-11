4181a67bc5bd6d0273efd2865b0cbcf660db82ef
Kowal <113093445+Doubleduck61@users.noreply.github.com>
2023-05-11 21:40:56 +0200

    Update 060.py

--- 060.py
+++ 060.py
@@ -11,21 +11,21 @@ price = {
     "jabłka": 1,
 }
 
-print(price["jajka"])
-
-products = ["jajka", "mleko", "chleb", "jajka", "masło", "jajka", "jabłka", "chleb"]
-to_buy = list(set(products))
-dlugosc=len(to_buy)
 a=0
-koszt=[]
-print("Lista produktów do kupienia: ")
-print("Towar:\t Szt.: \t Cena:")
+cost=[]
+products = ["jajka", "mleko", "chleb", "jajka", "masło", "jajka", "jabłka", "chleb"]
+
+to_buy = list(set(products))
+length=len(to_buy)
+print("Lists of product to buy: ")
+print("Iteam:\t Piece.: \t Price:")
 for i in products:
-    if a < dlugosc:
-        produkt=to_buy[a]
-        print(to_buy[a],"\t",products.count(produkt),"\t",(products.count(produkt))*(price[produkt]),"zł")
+    if a < length:
+        product=to_buy[a]
+        print(product,"\t",products.count(product),"\t\t",(products.count(product))*(price[product]),"zł")
         a+=1
-        koszt.append((products.count(produkt))*(price[produkt]))
-        koszt_calkowity=sum(koszt)
+        cost.append((products.count(product))*(price[product]))
+        total_cost=sum(cost)
         
-print("Należność za wszystko wynosi:",koszt_calkowity,"zł")
+print()
+print("Total cost is: ",total_cost,"zł")
