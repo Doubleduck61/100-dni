49512d91e3d6503af5727be0e9ea973bfaad3456
Kowal <113093445+Doubleduck61@users.noreply.github.com>
2023-05-14 16:36:47 +0200

    Update 060.py

--- 060.py
+++ 060.py
@@ -1,8 +1,8 @@
-#Przygotuj aplikację która.
-#Będzie miała wbudowanyk cennik wszystkich produktów.
-#Przygotuje listę zakupów, złożoną z unikalnych produktów oraz podanej obok liczby produktów do kupienia.
-#Wyliczy wartość zakupów i poda ją w podsumowaniu.
-#Przyposac funkcje do losty i wyswietlac loste?
+# Przygotuj aplikację która.
+# Będzie miała wbudowany cennik wszystkich produktów.
+# Przygotuje listę zakupów, złożoną z unikalnych produktów oraz podanej obok liczby produktów do kupienia.
+# Wyliczy wartość zakupów i poda ją w podsumowaniu.\
+
 price = {
     "jajka": 5,
     "chleb": 2,
@@ -11,21 +11,32 @@ price = {
     "jabłka": 1,
 }
 
-a=0
-cost=[]
+a = 0
+cost = []
 products = ["jajka", "mleko", "chleb", "jajka", "masło", "jajka", "jabłka", "chleb"]
 
-to_buy = list(set(products))
-length=len(to_buy)
+def lists(a, cost, products):
+    to_buy = list(set(products))
+    length = len(to_buy)
+    for i in products:
+        if a < length:
+            product = to_buy[a]
+            print(product, "\t", products.count(product), "\t\t", (products.count(product)) * (price[product]), "zł")
+            a += 1
+            if a < length:
+                all_money(a, cost, products)
+            else:
+                break
+    return ""
+            
+def all_money(a, cost, products):
+    to_buy = list(set(products))
+    product = to_buy[a]
+    cost.append((products.count(product)) * (price[product]))
+    total_cost = sum(cost)
+    return total_cost
+    
 print("Lists of product to buy: ")
-print("Iteam:\t Piece.: \t Price:")
-for i in products:
-    if a < length:
-        product=to_buy[a]
-        print(product,"\t",products.count(product),"\t\t",(products.count(product))*(price[product]),"zł")
-        a+=1
-        cost.append((products.count(product))*(price[product]))
-        total_cost=sum(cost)
-        
-print()
-print("Total cost is: ",total_cost,"zł")
+print("Iteam:\t Piece.: \t Price:\n")
+print(lists(a, cost, products))
+print("Total cost:", all_money(a, cost, products), "zł")
