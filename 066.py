4238d8a5864d7852bb9009774e29192e47928357
Kowal <113093445+Doubleduck61@users.noreply.github.com>
2023-05-21 19:57:33 +0200

    Update 066.py

--- 066.py
+++ 066.py
@@ -1,12 +1,12 @@
 file_path = "D:\IT\Python\Projekty\plik.txt"
-produkty = []
+f = open(file_path, "r")
 
 def otwarcie():
-    f = open(file_path, "r")
     zawartosc = f.readlines() #przypisanie zawartości pliku
     return zawartosc
 
-def stworzenie_slownika(zawartosc, produkty):
+def stworzenie_slownika(zawartosc):
+    produkty = []
     for i in zawartosc:
         produkty.append(i.split(',')) #przypisanie do listy - stworzono podział na produkt - cena
     slownik = dict(produkty)
@@ -16,5 +16,7 @@ def wywolanie_slownika(slownik):
     for i in slownik:
         if float(slownik[i]) < 8:
             print(i)
-
-print(wywolanie_slownika(stworzenie_slownika(otwarcie(), produkty)))
+try:
+    print(wywolanie_slownika(stworzenie_slownika(otwarcie())))
+finally:
+    f.close()
